0062aaa3d7399ca4cdf2e2efa2a2d4a0c0b34d9f
Jonathan Channon <jonathan.channon@gmail.com>
2015-12-22 10:33:17 +0000

    added coreclr version

--- Program.cs
+++ Program.cs
@@ -4,24 +4,30 @@ using System.Diagnostics;
 
 namespace MediatRTest
 {
-    class MainClass
+    public class Program
     {
         public static void Main(string[] args)
         {
+            ProfileMediatR();
+        }
+
+        private static void ProfileMediatR()
+        {
             var sw = new Stopwatch();
             sw.Start();
-            var mediator = new MediatR.Mediator((type) => new QueryHandler(), (type) => Enumerable.Empty<object>());
+            var mediator = new MediatR.Mediator(type => new QueryHandler(), type => Enumerable.Empty<object>());
             for (int i = 0; i < 10000; i++)
             {
                 var query = new Query();
-                var result = mediator.Send(query); 
+                var result = mediator.Send(query);
             }
             sw.Stop();
             var time = sw.ElapsedMilliseconds;
             Console.WriteLine(time);
-            Console.WriteLine("Done");
-            Console.ReadKey();
+            Console.WriteLine("MediatR Done");
+
         }
+
     }
 
     public class Query : MediatR.IRequest<int>
@@ -36,4 +42,5 @@ namespace MediatRTest
             return 0;
         }
     }
+
 }
